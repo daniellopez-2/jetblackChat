c982c39853b6ca01893cfdadb18c99d4ea056756
Quant Aurelius <qaurelius@nthorp.com>
2024-01-22 19:14:54 -0600

    login

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,161 +1,151 @@
-import { Brand } from "@/components/ui/brand"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { createClient } from "@/lib/supabase/server"
-import { Database } from "@/supabase/types"
-import { createServerClient } from "@supabase/ssr"
-import { Metadata } from "next"
-import { cookies, headers } from "next/headers"
-import { redirect } from "next/navigation"
+"use client";
 
-export const metadata: Metadata = {
-  title: "Login"
-}
+import Link from "next/link";
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Provider } from "@supabase/supabase-js";
+import toast from "react-hot-toast";
+import config from "@/config";
 
-export default async function Login({
-  searchParams
-}: {
-  searchParams: { message?: string }
-}) {
-  const cookieStore = cookies()
-  const supabase = createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        }
+// This a login/singup page for Supabase Auth.
+// Successfull login redirects to /api/auth/callback where the Code Exchange is processed (see app/api/auth/callback/route.js).
+export default function Login() {
+  const supabase = createClientComponentClient();
+  const [email, setEmail] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+  const handleSignup = async (
+    e: any,
+    options: {
+      type: string;
+      provider?: Provider;
+    }
+  ) => {
+    e?.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      const { type, provider } = options;
+      const redirectURL = window.location.origin + "/api/auth/callback";
+
+      if (type === "oauth") {
+        await supabase.auth.signInWithOAuth({
+          provider,
+          options: {
+            redirectTo: redirectURL,
+          },
+        });
+      } else if (type === "magic_link") {
+        await supabase.auth.signInWithOtp({
+          email,
+          options: {
+            emailRedirectTo: redirectURL,
+          },
+        });
+
+        toast.success("Check your emails!");
+
+        setIsDisabled(true);
       }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
-  )
-  const session = (await supabase.auth.getSession()).data.session
-
-  if (session) {
-    return redirect("/chat")
-  }
-
-  const signIn = async (formData: FormData) => {
-    "use server"
-
-    const email = formData.get("email") as string
-    const password = formData.get("password") as string
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password
-    })
-
-    if (error) {
-      return redirect(`/login?message=${error.message}`)
-    }
-
-    return redirect("/chat")
-  }
-
-  const signUp = async (formData: FormData) => {
-    "use server"
-
-    const email = formData.get("email") as string
-    const password = formData.get("password") as string
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
-        // emailRedirectTo: `${origin}/auth/callback`
-      }
-    })
-
-    if (error) {
-      console.error(error)
-      return redirect(`/login?message=${error.message}`)
-    }
-
-    return redirect("/login?message=Check your inbox to activate your account.")
-
-    // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
-    // return redirect("/login?message=Check email to continue sign in process")
-  }
-
-  const handleResetPassword = async (formData: FormData) => {
-    "use server"
-
-    const origin = headers().get("origin")
-    const email = formData.get("email") as string
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-
-    const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: `${origin}/auth/callback?next=/login/password`
-    })
-
-    if (error) {
-      return redirect(`/login?message=${error.message}`)
-    }
-
-    return redirect("/login?message=Check email to reset password")
-  }
+  };
 
   return (
-    <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
-      <form
-        className="animate-in text-foreground flex w-full flex-1 flex-col justify-center gap-2"
-        action={signIn}
-      >
-        <Brand />
-
-        <Label className="text-md mt-4" htmlFor="email">
-          Email
-        </Label>
-        <Input
-          className="mb-3 rounded-md border bg-inherit px-4 py-2"
-          name="email"
-          placeholder="Your Email Address"
-          required
-        />
-
-        <Label className="text-md" htmlFor="password">
-          Password
-        </Label>
-        <Input
-          className="mb-6 rounded-md border bg-inherit px-4 py-2"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-        />
-
-        <Button className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
-          Login
-        </Button>
-
-        <Button
-          formAction={signUp}
-          className="border-foreground/20 mb-2 rounded-md border px-4 py-2"
-        >
-          Sign Up
-        </Button>
-
-        <div className="text-muted-foreground mt-1 flex justify-center text-sm">
-          <span className="mr-1">Forgot your password?</span>
-          <button
-            formAction={handleResetPassword}
-            className="text-primary ml-1 underline hover:opacity-80"
+    <main className="p-8 md:p-24" data-theme={config.colors.theme}>
+      <div className="text-center mb-4">
+        <Link href="/" className="btn btn-ghost btn-sm">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
           >
-            Reset
-          </button>
+            <path
+              fillRule="evenodd"
+              d="M15 10a.75.75 0 01-.75.75H7.612l2.158 1.96a.75.75 0 11-1.04 1.08l-3.5-3.25a.75.75 0 010-1.08l3.5-3.25a.75.75 0 111.04 1.08L7.612 9.25h6.638A.75.75 0 0115 10z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Home
+        </Link>
+      </div>
+      <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight text-center mb-12">
+        Sign-in to {config.appName}{" "}
+      </h1>
+
+      <div className="space-y-8 max-w-xl mx-auto">
+        <button
+          className="btn btn-block"
+          onClick={(e) =>
+            handleSignup(e, { type: "oauth", provider: "google" })
+          }
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <span className="loading loading-spinner loading-xs"></span>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#FFC107"
+                d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"
+              />
+              <path
+                fill="#FF3D00"
+                d="m6.306 14.691 6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 16.318 4 9.656 8.337 6.306 14.691z"
+              />
+              <path
+                fill="#4CAF50"
+                d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"
+              />
+              <path
+                fill="#1976D2"
+                d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002 6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917z"
+              />
+            </svg>
+          )}
+          Sign-up with Google
+        </button>
+
+        <div className="divider text-xs text-base-content/50 font-medium">
+          OR
         </div>
 
-        <p className="bg-foreground/10 text-foreground mt-4 p-4 text-center">
-          {searchParams?.message || "Notice: Don't have an account? Sign up now!"}
-        </p>
-      </form>
-    </div>
-  )
+        <form
+          className="form-control w-full space-y-4"
+          onSubmit={(e) => handleSignup(e, { type: "magic_link" })}
+        >
+          <input
+            required
+            type="email"
+            value={email}
+            autoComplete="email"
+            placeholder="tom@cruise.com"
+            className="input input-bordered w-full placeholder:opacity-60"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <button
+            className="btn btn-primary btn-block"
+            disabled={isLoading || isDisabled}
+            type="submit"
+          >
+            {isLoading && (
+              <span className="loading loading-spinner loading-xs"></span>
+            )}
+            Send Magic Link
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 }
