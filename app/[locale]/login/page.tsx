79ed1c980294c5b4a6129f6c6ede882bbd4e6ca9
Quant Aurelius <qaurelius@nthorp.com>
2024-01-22 19:37:40 -0600

    signin

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,70 +1,161 @@
-// Import necessary modules
-import { useState } from "react"
-import { createClient } from "@supabase/supabase-js"
-import toast from "react-hot-toast"
+import { Brand } from "@/components/ui/brand"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { createClient } from "@/lib/supabase/server"
+import { Database } from "@/supabase/types"
+import { createServerClient } from "@supabase/ssr"
+import { Metadata } from "next"
+import { cookies, headers } from "next/headers"
+import { redirect } from "next/navigation"
 
-// Initialize the Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const metadata: Metadata = {
+  title: "Login"
+}
 
-export default function Login() {
-  const [email, setEmail] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-
-  const handleGoogleSignIn = async () => {
-    setIsLoading(true)
-    try {
-      const { error } = await supabase.auth.signInWithOAuth({
-        provider: "google",
-        options: { redirectTo: window.location.origin + "/api/auth/callback" }
-      })
-      if (error) throw error
-    } catch (error) {
-      console.error("Error with Google sign-in:", error.message)
-      toast.error(error.message)
-    } finally {
-      setIsLoading(false)
+export default async function Login({
+  searchParams
+}: {
+  searchParams: { message?: string }
+}) {
+  const cookieStore = cookies()
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        }
+      }
     }
+  )
+  const session = (await supabase.auth.getSession()).data.session
+
+  if (session) {
+    return redirect("/chat")
   }
 
-  const handleMagicLinkSignIn = async e => {
-    e.preventDefault()
-    setIsLoading(true)
-    try {
-      const { error } = await supabase.auth.signInWithOtp({
-        email,
-        options: {
-          emailRedirectTo: window.location.origin + "/api/auth/callback"
-        }
-      })
-      if (error) throw error
-      toast.success("Check your email for the login link!")
-    } catch (error) {
-      console.error("Error with Magic Link sign-in:", error.message)
-      toast.error(error.message)
-    } finally {
-      setIsLoading(false)
+  const signIn = async (formData: FormData) => {
+    "use server"
+
+    const email = formData.get("email") as string
+    const password = formData.get("password") as string
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+
+    if (error) {
+      return redirect(`/login?message=${error.message}`)
     }
+
+    return redirect("/chat")
+  }
+
+  const signUp = async (formData: FormData) => {
+    "use server"
+
+    const email = formData.get("email") as string
+    const password = formData.get("password") as string
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
+        // emailRedirectTo: `${origin}/auth/callback`
+      }
+    })
+
+    if (error) {
+      console.error(error)
+      return redirect(`/login?message=${error.message}`)
+    }
+
+    return redirect("/login?message=Check your inbox to activate your account.")
+
+    // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
+    // return redirect("/login?message=Check email to continue sign in process")
+  }
+
+  const handleResetPassword = async (formData: FormData) => {
+    "use server"
+
+    const origin = headers().get("origin")
+    const email = formData.get("email") as string
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: `${origin}/auth/callback?next=/login/password`
+    })
+
+    if (error) {
+      return redirect(`/login?message=${error.message}`)
+    }
+
+    return redirect("/login?message=Check email to reset password")
   }
 
   return (
-    <div className="login-container">
-      <button onClick={handleGoogleSignIn} disabled={isLoading}>
-        {isLoading ? "Loading..." : "Sign in with Google"}
-      </button>
+    <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
+      <form
+        className="animate-in text-foreground flex w-full flex-1 flex-col justify-center gap-2"
+        action={signIn}
+      >
+        <Brand />
 
-      <form onSubmit={handleMagicLinkSignIn}>
-        <input
-          type="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          placeholder="Enter your email"
+        <Label className="text-md mt-4" htmlFor="email">
+          Email
+        </Label>
+        <Input
+          className="mb-3 rounded-md border bg-inherit px-4 py-2"
+          name="email"
+          placeholder="Your Email Address"
           required
         />
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? "Loading..." : "Send Magic Link"}
-        </button>
+
+        <Label className="text-md" htmlFor="password">
+          Password
+        </Label>
+        <Input
+          className="mb-6 rounded-md border bg-inherit px-4 py-2"
+          type="password"
+          name="password"
+          placeholder="••••••••"
+        />
+
+        <Button className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
+          Login
+        </Button>
+
+        <Button
+          formAction={signUp}
+          className="border-foreground/20 mb-2 rounded-md border px-4 py-2"
+        >
+          Sign Up
+        </Button>
+
+        <div className="text-muted-foreground mt-1 flex justify-center text-sm">
+          <span className="mr-1">Forgot your password?</span>
+          <button
+            formAction={handleResetPassword}
+            className="text-primary ml-1 underline hover:opacity-80"
+          >
+            Reset
+          </button>
+        </div>
+
+        <p className="bg-foreground/10 text-foreground mt-4 p-4 text-center">
+          {searchParams?.message ||
+            "Notice: Don't have an account? Sign up now!"}
+        </p>
       </form>
     </div>
   )
