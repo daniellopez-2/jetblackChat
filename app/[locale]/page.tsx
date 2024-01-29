b4a722a2b11594529193dfcf6a33f7c49d950b0b
Quant Aurelius <qaurelius@nthorp.com>
2024-01-29 10:12:47 -0600

    branding

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -17,10 +17,10 @@ export default function HomePage() {
       <div className="mt-2 text-4xl mb-4"></div>
 
       <Link
-        className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
+        className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-white p-2 font-semibold"
         href="/chat"
       >
-        Go to App
+        AI Chat App
       </Link>
     </div>
   )
